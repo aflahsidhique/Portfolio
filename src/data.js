941093ba7503e1e81ca6f1e0c201386dcbd39ee3
aflahsidhique <aflahsidhique@gmail.com>
2023-11-03 00:00:25 +0530

    .

---
+++ src/data.js
@@ -0,0 +1,64 @@
+export const projects = [
+  {
+    title: "TechieGram",
+    subtitle: "Wordpress",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./tec.gif",
+    link: "https://buddyboss.aflahsidhique.ind.in/",
+  },
+  {
+    title: "ASHUB",
+    subtitle: "Html Css Js",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./ashub.png",
+    link: "https://aflahsidhique.github.io/ashub/",
+  },
+  {
+    title: "Axie Tracker",
+    subtitle: "Html Css Js",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./axie.png",
+    link: "https://aflahsidhique.github.io/axie-tracker/",
+  },
+  {
+    title: "Mulearn Dashboard",
+    subtitle: "Mern Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./mulearn.png",
+    link: "https://app.mulearn.org/profile/aflahsidhiquemk@mulearn",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Html",
+  "Css",
+  "JavaScript",
+  "React",
+  "Node.js",
+  "Java",
+  "Python",
+  "MongoDB",
+  "Django",
+  "MySQL",
+];
